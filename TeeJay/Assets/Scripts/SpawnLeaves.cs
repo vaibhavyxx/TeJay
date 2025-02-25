987345c9196ddaf8198d02fab301682b17ec1913
vaibhavyxx <vd5443@rit.edu>
2025-02-24 21:31:05 -0500

    spawns a new aloe vera every 0.01 seconds

--- TeeJay/Assets/Scripts/SpawnLeaves.cs
+++ TeeJay/Assets/Scripts/SpawnLeaves.cs
@@ -13,6 +13,11 @@ public class SpawnLeaves : MonoBehaviour
     int totalMeshes = 0;
     float deltaAngle = 0.0f;
     float meshWidth = 0.0f;
+    int counter = 0;
+
+    //time
+    float time = 0.0f;
+    const float deltaTime = 0.01f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,10 +27,10 @@ public class SpawnLeaves : MonoBehaviour
         totalMeshes = Mathf.RoundToInt(2* Mathf.PI * radius / meshWidth);
         deltaAngle = 2* Mathf.PI/ totalMeshes;
         coordinates = new Vector3[totalMeshes];
+
         //totalMeshes = int.Parse 2 * Mathf.PI * radius;
-        
         //finds the coordinates to make a circle
-        for(int i = 0; i < totalMeshes; i++)
+        /*for(int i = 0; i < totalMeshes; i++)
         {
             float angle = i * deltaAngle;
             float x = radius * Mathf.Cos(angle);
@@ -33,15 +38,32 @@ public class SpawnLeaves : MonoBehaviour
             //Debug.Log("angle "+ angle+" "+ x+ ", "+y);
             coordinates[i] = new Vector3(x, 0, z);
             Instantiate(meshObject, coordinates[i], Quaternion.identity);
-
-        }
-        Debug.Log("total meshes: " + totalMeshes);
+            //time = Time.time;
+            //Debug.Log(time);
+        }*/
+        //Debug.Log("total meshes: " + totalMeshes);
     }
      
 
     // Update is called once per frame
     void Update()
     {
+        time = Time.deltaTime;
+        Debug.Log("Time: "+ time);
+        if(time > deltaTime && counter < totalMeshes)
+        {
+            time = 0.0f;                                //resets time
+            float angle = counter * deltaAngle;
+            Vector3 coordinate = new Vector3(radius * Mathf.Cos(angle),
+                counter, radius * Mathf.Sin(angle));
+            makeObject(coordinate, Quaternion.identity);
+            counter++;
+        }
         
+    }
+
+    void makeObject(Vector3 coordinates, Quaternion rotation)
+    {
+        Instantiate(meshObject, coordinates, rotation);
     }
 }
