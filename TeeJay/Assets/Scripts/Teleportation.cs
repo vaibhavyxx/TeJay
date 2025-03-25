32aedc5731180c055d3a9f117c9a9aeea883bc78
vaibhavyxx <vd5443@rit.edu>
2025-03-25 12:33:25 -0400

    assets

---
+++ TeeJay/Assets/Scripts/Teleportation.cs
@@ -0,0 +1,25 @@
+//Helps in travelling from one portal to another
+using UnityEngine;
+public class Teleportation : MonoBehaviour
+{
+    [SerializeField] GameObject otherPortal;
+    [SerializeField] GameObject player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player.transform.position = otherPortal.transform.position ;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
