32c28878a8aa54611df48565065450be5a13c433
vaibhavyxx <vd5443@rit.edu>
2025-04-01 12:02:00 -0400

    working with player input, removed excess assets

--- TeeJay/Assets/Scripts/PlayerLook.cs
+++ TeeJay/Assets/Scripts/PlayerLook.cs
@@ -25,7 +25,7 @@ public class PlayerLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
+        xRotation -= mouseX;
         xRotation = Mathf.Clamp(xRotation, -90f, minViewDistance);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX); 
--- TeeJay/Assets/Scripts/PlayerMove.cs
+++ TeeJay/Assets/Scripts/PlayerMove.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 //Code is based on video https://www.youtube.com/watch?v=o1bj-49uQ74
-//The code given didn't work that well, so used chatGPT to fix up the code.
+
 public class PlayerMove : MonoBehaviour
 {
     //Movement of the player body.
@@ -17,25 +17,23 @@ public class PlayerMove : MonoBehaviour
 
     //gravity
     public float gravity = 100f;
-    float time = 0.0f;
-
+    Time time = null;
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
         //myRigidbody.freezeRotation = true; // Prevent unwanted rotation
-
-        //get current time
-        time = Time.time;
-
     }
-
     // Conditions for jumping, space bar to jump.
     void Update()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
+        /*if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
         {
             Jump();
         }
+
+        //Rotation
+        Debug.Log("Rotation: "+ transform.rotation.ToEulerAngles());
+        Debug.Log("Forward: " + transform.forward);*/
     }
 
     //Move the player x or y axis
@@ -43,41 +41,53 @@ public class PlayerMove : MonoBehaviour
     {
         Run();
         ApplyGravity();
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
+        {
+            Jump();
+        }
     }
 
     // Our movement of x and y axis.
     void Run()
     {
-        Vector3 moveDirection = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
-        //Vector3 playerForce = moveDirection * walkSpeed;
-        Vector3 newVelocity = new Vector3(moveDirection.x * walkSpeed,
-            myRigidbody.linearVelocity.y, moveDirection.z * walkSpeed);
-        myRigidbody.linearVelocity = newVelocity;
-        // Preserve Y velocity (gravity)
-        // myRigidbody.linearVelocity = new Vector3(playerVelocity.x, myRigidbody.linearVelocity.y, playerVelocity.z);
-        //myRigidbody.AddForce(playerForce, ForceMode.Force);
+        //Changed it from forceMode to ensure gravity still works when rigidbody moves
+        Vector3 moveDirection = (transform.right * moveInput.x + transform.forward * moveInput.y);//transform.up.normalized;
+            //new Vector3(moveInput.x, 0f, moveInput.y).normalized;
+        Vector3 newVelocity = new Vector3(
+            moveDirection.x * walkSpeed,
+            myRigidbody.linearVelocity.y, 
+            moveDirection.z * walkSpeed);
+        myRigidbody.linearVelocity = (newVelocity);
+        Debug.Log("rb velocity: " + myRigidbody.linearVelocity);
     }
 
     // Jump power for our player
     void Jump()
     {
-        myRigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        myRigidbody.AddForce(transform.up * jumpPower, ForceMode.Impulse);
         isGrounded = false;
     }
 
     // In order for the player to jump, the player must land on platform with the tage "Grounded". No infinite jumps.
-    void OnCollisionStay(Collision collision) // Corrected function name
+    void OnCollisionStay(Collision collision) 
     {
         
-        if (collision.gameObject.CompareTag("Grounded")) // Make sure the ground has this tag
+        if (collision.gameObject.CompareTag("Grounded")) 
         {
             isGrounded = true;
         }
     }
 
-    void OnMove(InputValue value)
+    //shouldn't it be get context?
+    public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+        Debug.LogWarning("Move Input: " + moveInput);
+        if(moveInput.magnitude > 1)
+        {
+            moveInput.Normalize();
+        }
     }
 
     //Ensures you fall properly
@@ -87,5 +97,11 @@ public class PlayerMove : MonoBehaviour
         {
             myRigidbody.AddForce(Vector3.down * (Physics.gravity.y * gravity), ForceMode.Acceleration);
         }
+    }
+
+    //Updates direction's axis
+    private Vector3 newAxis()
+    {
+        return Vector3.zero;
     }
 }
