8c500b14b21a4b987f946c10a5dd17ec5e44688f
jm1060 <jm1060@rit.edu>
2025-04-17 13:43:12 -0400

    Gamespawn finally works
    Gamespawn works, Player falls off after -175 in y-axis

--- TeeJay/Assets/Scripts/GameRespawn.cs
+++ TeeJay/Assets/Scripts/GameRespawn.cs
@@ -1,22 +1,30 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 //Based on the video https://www.youtube.com/watch?v=Mic9ERhr0HA
 public class GameRespawn : MonoBehaviour
 {
    
     public float threshold;
-
+    [SerializeField] private Vector3 startingposition;
     // Update is called once per frame
+    void Start()
+    {
+        startingposition = this.transform.localPosition;
+        
+    }
     void Update()
     {
         //If the player falls lower than the threshold, it will respawn at the coordinates listed below
         //in the code.
-        if(transform.position.y < threshold)
+        
+        if(this.transform.localPosition.y < threshold)
         {
-            Debug.Log("Player has fallen less then "+threshold);
-            transform.position = new Vector3(610.6f, -26.2f,-536.8f);
-            Debug.Log("Respawn point: "+transform.position.x + " "+ transform.position.y+ " "+ transform.position.z);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            // Debug.Log("Player has fallen less then "+threshold);/
+            this.transform.localPosition = startingposition;
+            Debug.Log(gameObject.name + " Respawn point: "+transform.localPosition.x + " "+ transform.localPosition.y+ " "+ transform.localPosition.z);
         }
     }
 }
