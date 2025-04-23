85d61b745479dc2f01f0ff380d5f884712c9709e
vaibhavyxx <vd5443@rit.edu>
2025-04-23 19:50:40 -0400

    haptic feedback for grab but acts weird

--- TeeJay/Assets/Scripts/GrabController.cs
+++ TeeJay/Assets/Scripts/GrabController.cs
@@ -8,16 +8,20 @@ using UnityEngine.Pool;
 //then the object would flag as grabbed
 //then the object, transform would be the new transform
 //Make sure that the gameobject this script is attached to, has a collider with trigger checked
-
 public class GrabController : MonoBehaviour
 {
-    public Transform grabObject;                //grab object's coordinates
-    public Rigidbody grabRigidBody;             //grab object's rigidbody to enable/disable gravity
+    Transform grabObject;                       //grab object's coordinates
+    Rigidbody grabRigidBody;                    //grab object's rigidbody to enable/disable gravity
     public Transform grabTransform;             //grabbed object is held at this coordinate
 
     //Boolean flags to ensure they work properly
     bool isGrabbed = false;
     bool isTrigger = false;
+
+    //Plays haptics to let user know if they have grabbed something
+    bool playsHaptics = false;
+    float timer = 0.0f;
+    float waitTime = 2.0f;
 
     //Takes in B and Y input for grab and drop
     ButtonControl currentInput;
@@ -25,6 +29,7 @@ public class GrabController : MonoBehaviour
 
     void Update()
     {
+        //timer += Time.deltaTime;                 //for timing haptics
         currentInput = Gamepad.current.bButton;  //takes in value from each b button - to grab item
         yInput = Gamepad.current.yButton;        //to drop the item
 
@@ -49,16 +54,47 @@ public class GrabController : MonoBehaviour
         {
             isGrabbed=true;
         }
+
+        //for haptics
+        FeelTriggers(0.25f);
     }
 
     //Checks for multiple frames to ensure we can get information about the collider
     //whenever the object is pressed
     private void OnTriggerStay(Collider other)
     {
-        //only grabs if the item is within the radius and the button is pressed
-        if (other.gameObject.CompareTag("Grab") && isGrabbed)
+        if (other.gameObject.CompareTag("Grab"))
         {
-            isTrigger = true;
+            //identifies the object's transform and rb
+            grabObject = other.GetComponent<Transform>();
+            grabRigidBody = other.GetComponent<Rigidbody>();
+
+            //only grabs if the item is within the radius and the button is pressed
+            if (isGrabbed)
+            {
+                isTrigger = true;
+                playsHaptics=true;
+            }
+        }
+    }
+
+    //counts time and ensure the haptics are triggered for 0.25s
+    void FeelTriggers(float playTime)
+    {
+        if (playsHaptics)
+        {
+            Gamepad.current.SetMotorSpeeds(0.25f, 0.25f);
+            timer += Time.deltaTime;
+
+            if(timer > playTime)
+            {
+                playsHaptics = false;
+                InputSystem.PauseHaptics();
+            }
+        }
+        else
+        {
+            timer = 0.0f;
         }
     }
   
